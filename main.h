c198edd78a35a042278b4e58885d6e9341b949bc
zap9412 <thelyaan444@hotmail.com>
2025-11-26 13:36:48 +0100

    C - printf

--- main.h
+++ main.h
@@ -1,26 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
+#include <limits.h>
+#include <stdio.h>
 #include <stdarg.h>
-#include <stddef.h>
+#include <unistd.h>
+#include "main.h"
 
-/* Type pour lier un caractère de spécificateur à une fonction */
-typedef int (*handler_t)(va_list);
-
-/* Structure pour la table de spécificateurs */
-typedef struct specifier
-{
-    char type;
-    handler_t func;
-} specifier_t;
-
-/* Prototypes */
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(const char *s);
+int handle_char(va_list args);
+int handle_string(va_list args);
+int handle_percent(void);
 
-/* Fonctions gérées (handlers) et lookup */
-int (*get_specifier_func(char c))(va_list);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-
-#endif /* MAIN_H */
+#endif
